42f6859f4230acf1f9ec398faddcb855a2ee56fd
gauravm03 <82034954+gauravm03@users.noreply.github.com>
2023-06-27 02:01:12 +0530

    committed

---
+++ script.js
@@ -0,0 +1,89 @@
+
+// buybutt
+const sellbutt=document.querySelector("#sellbutt");
+const sellmenu=document.querySelector(".sell_menu");
+
+
+
+sellbutt.addEventListener("mouseover",()=>{
+    
+   
+    sellmenu.classList.toggle('active');
+});
+
+sellbutt.addEventListener("mouseout",()=>{
+   
+
+   
+    sellmenu.classList.toggle('active');
+});
+
+sellmenu.addEventListener("mouseover",()=>{
+    
+   
+    sellmenu.classList.toggle('active');
+});
+
+sellmenu.addEventListener("mouseout",()=>{
+   
+
+   
+    sellmenu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+// sellbutt
+
+
+
+
+
+const buybutt=document.querySelector("#buybutt");
+const buymenu=document.querySelector(".buy_menu");
+
+
+
+
+buybutt.addEventListener("mouseover",()=>{
+    
+   
+    buymenu.classList.toggle('active');
+});
+
+buybutt.addEventListener("mouseout",()=>{
+   
+
+   
+    buymenu.classList.toggle('active');
+});
+
+buymenu.addEventListener("mouseover",()=>{
+    
+    buymenu.classList.toggle('active')
+
+});
+
+buymenu.addEventListener("mouseout",()=>{
+   
+
+   
+    buymenu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
